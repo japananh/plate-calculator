7bb9c25ecc82ee48cd16b3234271ffc5c7eb6dbf
japananh <japananh@gmail.com>
2022-02-13 22:45:01 +0700

    init commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+// use the express-static middleware
+app.use(express.static("public"));
+
+// define the first route
+app.get("/", function (req, res) {
+  res.send("/pubic/index.html");
+});
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
